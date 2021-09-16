14749beae1d9d2a3e9e97221606139a430d21532
Sanninm <mudzelet.sanin@gmail.com>
2021-09-16 15:34:31 +0200

    Decimal Numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,20 @@
+function main (){
+
+     // Only change code below this line 
+
+     myDecimal = 5.5;
+     myDecemalE = 2 ** 5;
+    
+     // Only change code above this line
+    
+     return {
+
+         myDecimal,
+         myDecemalE
+     };
+    
+    }
+
+    console.log(main());
+    
+    module.exports = main;
